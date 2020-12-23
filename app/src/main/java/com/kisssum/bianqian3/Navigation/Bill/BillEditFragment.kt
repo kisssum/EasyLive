3b14e4b9e8e1ae0d8f11edcdb143154a21fffe4b
sea <1284700369@qq.com>
2020-12-23 21:31:59 +0800

    设置账单编辑页的取消按钮事件

--- app/src/main/java/com/kisssum/bianqian3/Navigation/Bill/BillEditFragment.kt
+++ app/src/main/java/com/kisssum/bianqian3/Navigation/Bill/BillEditFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigation.NavigationView
 import com.kisssum.bianqian3.R
+import com.kisssum.bianqian3.databinding.FragmentBillEditBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +26,8 @@ class BillEditFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentBillEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,9 +39,17 @@ class BillEditFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bill_edit, container, false)
+    ): View {
+        binding = FragmentBillEditBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCancel.setOnClickListener {
+            Navigation.findNavController(requireActivity(), R.id.fragment_main).navigateUp()
+        }
     }
 
     companion object {

df6c1d623517d25351251bddb081afb0b3d4da4a
sea <1284700369@qq.com>
2021-01-10 22:56:06 +0800

    通知栏颜色适配

--- app/src/main/java/com/kisssum/bianqian3/MainActivity.kt
+++ app/src/main/java/com/kisssum/bianqian3/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kisssum.bianqian3
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }

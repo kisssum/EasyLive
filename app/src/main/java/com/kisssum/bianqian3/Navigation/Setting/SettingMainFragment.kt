0e2e261c2a221ddc3bb8ff5722952998b68add9c
sea <1284700369@qq.com>
2021-01-12 20:50:52 +0800

    添加设置页面，实现清空账单功能

--- app/src/main/java/com/kisssum/bianqian3/Navigation/Setting/SettingMainFragment.kt
+++ app/src/main/java/com/kisssum/bianqian3/Navigation/Setting/SettingMainFragment.kt
@@ -1,17 +1,17 @@
-package com.kisssum.bianqian3
+package com.kisssum.bianqian3.Navigation.Setting
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.kisssum.bianqian3.Navigation.Bill.BillMainFragment
-import com.kisssum.bianqian3.Navigation.Meno.MenoMainFragment
-import com.kisssum.bianqian3.Navigation.Setting.SettingMainFragment
-import com.kisssum.bianqian3.databinding.FragmentTabControlBinding
+import androidx.lifecycle.ViewModelProvider
+import com.kisssum.bianqian3.Navigation.Bill.BillViewModel
+import com.kisssum.bianqian3.R
+import com.kisssum.bianqian3.databinding.FragmentBillMainBinding
+import com.kisssum.bianqian3.databinding.FragmentSettingMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,14 +20,16 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TabControlFragment.newInstance] factory method to
+ * Use the [SettingMainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TabControlFragment : Fragment() {
+class SettingMainFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentTabControlBinding
+
+    private lateinit var binding: FragmentSettingMainBinding
+    private lateinit var billViewModel: BillViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,29 +43,28 @@ class TabControlFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentTabControlBinding.inflate(inflater)
+        binding = FragmentSettingMainBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.viewPager.adapter = object : FragmentStateAdapter(requireActivity()) {
-            override fun getItemCount() = 3
-            override fun createFragment(position: Int) = when (position) {
-                0 -> BillMainFragment()
-                1 -> MenoMainFragment()
-                else -> SettingMainFragment()
-            }
-        }
+        billViewModel = ViewModelProvider(
+            requireActivity(),
+            ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
+        ).get(BillViewModel::class.java)
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab: TabLayout.Tab, i: Int ->
-            tab.text = when (i) {
-                0 -> "账单"
-                1 -> "便签"
-                else -> "设置"
-            }
-        }.attach()
+        binding.btnClearBill.setOnClickListener {
+            AlertDialog.Builder(requireContext())
+                .setMessage("是否删除所有账单?")
+                .setPositiveButton("确认") { dialogInterface: DialogInterface, i: Int ->
+                    billViewModel.getBillDao().delAll()
+                }
+                .setNeutralButton("取消") { dialogInterface: DialogInterface, i: Int -> }
+                .create()
+                .show()
+        }
     }
 
     companion object {
@@ -73,12 +74,12 @@ class TabControlFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TabControlFragment.
+         * @return A new instance of fragment SettingMainFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TabControlFragment().apply {
+            SettingMainFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
